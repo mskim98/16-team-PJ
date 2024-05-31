09b853a21f2c67cf9a9df48d3196156683cba1fa
SH4316 <soohyuk4316@gmail.com>
2024-06-01 06:56:57 +0900

    Update place.ts

--- dreamtrip/src/pages/api/post/place.ts
+++ dreamtrip/src/pages/api/post/place.ts
@@ -5,7 +5,7 @@ import { apiKey } from "../../../util/api";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === "POST") {
         const placeList = req.body.list;
-        const result = {urls: []};
+        const result = {list: placeList, urls: []};
         for (let i = 0; i < placeList.length; i++) {
             const u = await dall23(`${placeList[i].place}에서 ${placeList[i].contents}를 하는 사람을 그려줘`, apiKey);
             result.urls.push(u);
