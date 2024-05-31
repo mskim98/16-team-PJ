51d4831eaecd7e8c07ef75c5edc719763d83f43c
SH4316 <soohyuk4316@gmail.com>
2024-06-01 06:54:07 +0900

    Feat: API region

--- dreamtrip/src/pages/api/post/place.ts
+++ dreamtrip/src/pages/api/post/place.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { dall23 } from "../../images";
-import { apiKey } from "../../../../util/api";
+import { dall23 } from "../images";
+import { apiKey } from "../../../util/api";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === "POST") {
